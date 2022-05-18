f713ded731efa6881dd4640ea10bd957d3561c16
mefengl <71683364+mefengl@users.noreply.github.com>
2022-05-18 15:31:06 +0800

    zz

--- WeReadHelper.js
+++ WeReadHelper.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         📘微信读书阅读助手
 // @namespace   https://github.com/mefengl
-// @version      5.2.1
+// @version      5.2.2
 // @description  现有功能✔：功能1️⃣：自动隐藏顶栏和侧边栏📌；功能2️⃣：半透明顶栏和侧边栏🦋；功能3️⃣：一键搜豆瓣、得到电子书，还可在孔夫子、多抓鱼买二手👁
 // @author       mefengl
 // @match        https://weread.qq.com/*
