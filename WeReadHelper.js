5a4707f435229ef01e8704667c202844c55fe5c7
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-09-14 16:57:19 +0800

    chore: Update README.md

--- WeReadHelper.js
+++ WeReadHelper.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         📘微信读书阅读助手
 // @namespace   https://github.com/mefengl
-// @version      6.4.10
+// @version      6.4.11
 // @description  读书人用的脚本
 // @author       mefengl
 // @match        https://weread.qq.com/*
